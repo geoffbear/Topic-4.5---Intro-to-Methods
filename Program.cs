3c8979a33e843f9337838d6170c2fa2dd21533a5
Geoffrey Dolbear <339470494@GoTVDSB.ca>
2024-03-07 14:23:20 -0500

    final

--- Program.cs
+++ Program.cs
@@ -6,56 +6,9 @@ namespace Topic_4._5___Intro_to_Methods
     {
         static void Main(string[] args)
         {
-            Random generator = new Random();
-            int num = generator.Next(1, 4);
-            Console.WriteLine("Welcome to Joke Boat would you like art or joke? (Type 'J' for a joke and 'A' for art)");
-            string answer = Console.ReadLine();
+            Welcome();
+
             
-            if (answer == "J")
-            {
-                if (num == 1)
-                {
-                    Welcome();
-                }
-                if (num == 2)
-                {
-                    Welcome();
-                }
-                else
-                {
-                    Welcome();
-                }
-            }
-
-            if (answer == "A")
-            {
-                if (num == 1)
-                {
-                    DrawVenusaur();
-                    Welcome();
-                }
-
-                if (num == 2)
-                {
-                    DrawCaterpie();
-                    Welcome();
-                }
-
-                else
-                {
-                    DrawCharizard();
-                    Welcome();
-                }
-            }
-
-            static void Welcome()
-            {
-                Random generator = new Random();
-                int num = generator.Next(1, 4);
-                Console.WriteLine();
-                Console.WriteLine("Welcome to Joke Boat would you like art or joke? (Type 'J' for a joke and 'A' for art");
-                string answer = Console.ReadLine();
-            }
 
         }
         static void DrawVenusaur()
@@ -73,5 +26,84 @@ namespace Topic_4._5___Intro_to_Methods
             string charizard = "\r\n                 .\"-,.__\r\n                 `.     `.  ,\r\n              .--'  .._,'\"-' `.\r\n             .    .'         `'\r\n             `.   /          ,'\r\n               `  '--.   ,-\"'\r\n                `\"`   |  \\\r\n                   -. \\, |\r\n                    `--Y.'      ___.\r\n                         \\     L._, \\\r\n               _.,        `.   <  <\\                _\r\n             ,' '           `, `.   | \\            ( `\r\n          ../, `.            `  |    .\\`.           \\ \\_\r\n         ,' ,..  .           _.,'    ||\\l            )  '\".\r\n        , ,'   \\           ,'.-.`-._,'  |           .  _._`.\r\n      ,' /      \\ \\        `' ' `--/   | \\          / /   ..\\\r\n    .'  /        \\ .         |\\__ - _ ,'` `        / /     `.`.\r\n    |  '          ..         `-...-\"  |  `-'      / /        . `.\r\n    | /           |L__           |    |          / /          `. `.\r\n   , /            .   .          |    |         / /             ` `\r\n  / /          ,. ,`._ `-_       |    |  _   ,-' /               ` \\\r\n / .           \\\"`_/. `-_ \\_,.  ,'    +-' `-'  _,        ..,-.    \\`.\r\n.  '         .-f    ,'   `    '.       \\__.---'     _   .'   '     \\ \\\r\n' /          `.'    l     .' /          \\..      ,_|/   `.  ,'`     L`\r\n|'      _.-\"\"` `.    \\ _,'  `            \\ `.___`.'\"`-.  , |   |    | \\\r\n||    ,'      `. `.   '       _,...._        `  |    `/ '  |   '     .|\r\n||  ,'          `. ;.,.---' ,'       `.   `.. `-'  .-' /_ .'    ;_   ||\r\n|| '              V      / /           `   | `   ,'   ,' '.    !  `. ||\r\n||/            _,-------7 '              . |  `-'    l         /    `||\r\n. |          ,' .-   ,' ||               | .-.        `.      .'     ||\r\n `'        ,'    `\".'    |               |    `.        '. -.'       `'\r\n          /      ,'      |               |,'    \\-.._,.'/'\r\n          .     /        .               .       \\    .''\r\n        .`.    |         `.             /         :_,'.'\r\n          \\ `...\\   _     ,'-.        .'         /_.-'\r\n           `-.__ `,  `'   .  _.>----''.  _  __  /\r\n                .'        /\"'          |  \"'   '_\r\n               /_|.-'\\ ,\".             '.'`__'-( \\\r\n                 / ,\"'\"\\,'               `/  `-.|\" mh";
             Console.Write(charizard);
         }
+
+        static void Welcome()
+        {
+            Console.WriteLine();
+            Random generator = new Random();
+            int num = generator.Next(1, 4);
+            Console.WriteLine("Welcome to Joke Boat would you like art or joke? (Type 'J' for a joke and 'A' for art or 'Q' to quit)");
+            string answer = Console.ReadLine();
+
+            if (answer == "J" || answer == "j")
+            { 
+                    Joke(num);
+                    Welcome();
+            }
+
+            else if (answer == "A" || answer == "a")
+            {
+                if (num == 1)
+                {
+                    DrawVenusaur();
+                    Welcome();
+                }
+
+                else if (num == 2)
+                {
+                    DrawCaterpie();
+                    Welcome();
+                }
+
+                else
+                {
+                    DrawCharizard();
+                    Welcome();
+                }
+            }
+
+            
+        }
+
+        static void Joke(int joke)
+        {
+            if (joke == 1)
+            {
+                Console.WriteLine("Knock knock");
+                Thread.Sleep(1000);
+                Console.WriteLine("Who's there");
+                Thread.Sleep(1000);
+                Console.WriteLine("Ice Cream");
+                Thread.Sleep(1000);
+                Console.WriteLine("Ice Cream who?");
+                Thread.Sleep(1000);
+                Console.WriteLine("Ice cream every time I see a scary movie!");
+            }
+            else if (joke == 2)
+            {
+                Console.WriteLine("Knock knock");
+                Thread.Sleep(1000);
+                Console.WriteLine("Who's there");
+                Thread.Sleep(1000);
+                Console.WriteLine("Boo");
+                Thread.Sleep(1000);
+                Console.WriteLine("Boo who?");
+                Thread.Sleep(1000);
+                Console.WriteLine("Don't cry, it's just a joke!");
+            }
+            else
+            {
+                Console.WriteLine("Knock knock");
+                Thread.Sleep(1000);
+                Console.WriteLine("Who's there");
+                Thread.Sleep(1000);
+                Console.WriteLine("Thermos");
+                Thread.Sleep(1000);
+                Console.WriteLine("Thermos who?");
+                Thread.Sleep(1000);
+                Console.WriteLine("Thermos be a better knock knock joke than this!");
+            }
+        }
+
     }
 }
